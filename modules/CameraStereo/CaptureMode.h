d501d74047e7a8d7e18c63da9c9dfb1e94cc952c
Max Graff <maksymilian.graff@gmail.com>
2025-12-05 22:34:37 +0100

    CaptureMode enum

---
+++ modules/CameraStereo/CaptureMode.h
@@ -0,0 +1,15 @@
+#ifndef CAMERA_RES_H
+#define CAMERA_RES_H
+
+enum CaptureMode
+{
+    HIGH = 2, NORMAL = 1, LOW = 0
+};
+
+/*
+    640 x 430 /
+    2304 x 1296 / 56fps
+    1536 x 864 / 120fps
+*/
+
+#endif
