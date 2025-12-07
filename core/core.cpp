ec71a7ea1cc66566b1a351bc85650e835ce69dd0
Max Graff <maksymilian.graff@gmail.com>
2025-12-07 17:04:05 +0100

    Core with stereoCam test

--- core/core.cpp
+++ core/core.cpp
@@ -1,7 +1,23 @@
 #include "CameraStereo.h"
 
+void take_and_send(CameraStereo*);
+
 int main(int argv, char** argc)
 {
     CameraStereo *stereoCam = new CameraStereo();
+    stereoCam->init();
+    stereoCam->start();
+
+
+    
     return 0;
+}
+
+void take_and_send(CameraStereo *cam)
+{
+    void *left = cam->getCamLeft()->frameMemory;
+    void *right = cam->getCamRight()->frameMemory;
+
+
+
 }
